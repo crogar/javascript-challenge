9ddb77b27bdfc39fa599ccd22e433d5ac7879c1c
crogar <58400421+crogar@users.noreply.github.com>
2021-04-27 23:22:01 -0400

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var filter_btn = d3.select("#filter-btn")
+var tbody = d3.select("#ufo-table")
+function populate_table(){
+
+}
+
+
+
+function filter_events(){
+    console.log("clicked btn")
+    console.log(d3.event.target);
+}
+
+
+filter_btn.on("click",filter_events);
